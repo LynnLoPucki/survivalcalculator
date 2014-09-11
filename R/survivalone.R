cc6014d28619d7687b9da111d429d71159d85be4
David Schnell-Davis <dschnelldavis@gmail.com>
2014-09-11 12:57:46 -0700

    update

--- R/survivalone.R
+++ R/survivalone.R
@@ -6,11 +6,11 @@
 #' @param saleintended2var. Optional.
 survivalone <- function(input) {
 	inputdata <- as.data.frame(input)
-	mylogit <- glm(emerge ~ saleintended2, data=successdata, family=binomial)
-	newdata1 <- with(successdata, data.frame(saleintended2=1))
+	mylogit <- glm(emerge ~ names(inputdata), data=successdata, family=binomial)
+	newdata1 <- with(successdata, inputdata)
 	newdata2 <- predict(mylogit, newdata=newdata1, type="response", se.fit=TRUE)
 	efit = newdata2$fit
 	lfit = efit - (newdata2$se.fit * 1.96)
 	hfit = efit + (newdata2$se.fit * 1.96)
-	list(efit=efit, hfit=hfit, lfit=lfit, stuff=inputdata)
+	list(efit=efit, hfit=hfit, lfit=lfit, stuff=names(inputdata))
 }
