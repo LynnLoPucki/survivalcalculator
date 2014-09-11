58613b0b37b5d23000996f258a954eb682c46b7d
David Schnell-Davis <dschnelldavis@gmail.com>
2014-09-10 17:03:33 -0700

    First test "success" function.

--- R/success.R
+++ R/success.R
@@ -1,36 +1,28 @@
-#' Hello World
+#' Success
 #' 
-#' Basic hello world function to be called from the demo app
+#' Test success function
 #' 
 #' @export
-#' @param myname your name. Required.
-hello <- function(myname = ""){
-  if(myname == ""){
-    stop("Tell me your name!")
-  }
-  list(
-    message = paste("hello", myname, "! This is", R.Version()$version.string)
-  )
+#' @param saleintended2. Optional.
+hello <- function(saleintended2var = 1) {
+# The variables names after "~" come from the calculator input.
+	mylogit <- glm(emerge ~ saleintended2 + ebitbeforedummy + equitybefore +  manufacturing, data=successdata, family=binomial)
+#	summary(mylogit)
+
+# The values after "=" come from the calculator input.
+	newdata1 <- with(successdata, data.frame(saleintended2=0, equitybefore=0, ebitbeforedummy=1, manufacturing=1))
+	newdata2 <- predict(mylogit, newdata=newdata1, type="response", se.fit=TRUE)
+
+# This is the predicted probability of Success
+	efit = newdata2$fit
+# These are the 95% lower and upper confidence intervals of efit
+	lfit = efit - (newdata2$se.fit * 1.96)
+	hfit = efit + (newdata2$se.fit * 1.96)
+
+# These are the values that should be returned to the user.
+#	efit # This is the predicted value
+#	lfit # This is the lower bound
+#	hfit # This is the upper bound
+
+	list( message = paste(efit, ",", hfit, ",", lfit) )
 }
-
-
-mydata <- read.csv("data/successdata.csv")
-
-#The variables names after "~" come from the calculator input.
-mylogit <- glm(emerge ~ saleintended2 + ebitbeforedummy + equitybefore +  manufacturing, data=mydata, family=binomial)
-summary(mylogit)
-
-#The values after "=" come from the calculator input.
-newdata1 <-with(mydata, data.frame(saleintended2=0, equitybefore=0, ebitbeforedummy = 1, manufacturing=1))
-newdata2 <- predict(mylogit, newdata=newdata1, type="response", se.fit=TRUE)
-
-#This is the predicted probability of Success
-efit = newdata2$fit
-#These are the 95% lower and upper confidence intervals of efit
-lfit = efit - (newdata2$se.fit * 1.96)
-hfit = efit + (newdata2$se.fit * 1.96)
-
-#these are the values that should be returned to the user.
-efit # This is the predicted value
-lfit # This is the lower bound
-hfit # This is the upper bound
