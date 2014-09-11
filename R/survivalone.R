02600d4c7f43c20b9a0951b70943a2619c9116da
David Schnell-Davis <dschnelldavis@gmail.com>
2014-09-11 11:34:43 -0700

    Added stuff

--- R/survivalone.R
+++ R/survivalone.R
@@ -4,15 +4,13 @@
 #' 
 #' @export
 #' @param saleintended2var. Optional.
-survivalone <- function(saleintended2var = "") {
-	if (saleintended2var == "") {
-		stop("You must enter a value for at least one variable.")
-	}
+survivalone <- function(input) {
+	inputdata <- as.data.frame(input)
 	mylogit <- glm(emerge ~ saleintended2, data=successdata, family=binomial)
-	newdata1 <- with(successdata, data.frame(saleintended2=[[saleintended2var]]))
+	newdata1 <- with(successdata, data.frame(saleintended2=1))
 	newdata2 <- predict(mylogit, newdata=newdata1, type="response", se.fit=TRUE)
 	efit = newdata2$fit
 	lfit = efit - (newdata2$se.fit * 1.96)
 	hfit = efit + (newdata2$se.fit * 1.96)
-	list(efit=efit, hfit=hfit, lfit=lfit)
+	list(efit=efit, hfit=hfit, lfit=lfit, stuff=names(inputdata))
 }
