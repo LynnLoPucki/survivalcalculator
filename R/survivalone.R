43f821e951756e736fa63f25ce227f234991027d
David Schnell-Davis <dschnelldavis@gmail.com>
2014-09-11 11:26:57 -0700

    update

--- R/survivalone.R
+++ R/survivalone.R
@@ -4,14 +4,12 @@
 #' 
 #' @export
 #' @param saleintended2var. Optional.
-#' @param ebitbeforedummyvar. Optional.
 survivalone <- function(saleintended2var = "") {
 	if (saleintended2var == "") {
 		stop("You must enter a value for at least one variable.")
-	} else {
-		mylogit <- glm(emerge ~ saleintended2, data=successdata, family=binomial)
-		newdata1 <- with(successdata, data.frame(saleintended2=1))
 	}
+	mylogit <- glm(emerge ~ saleintended2, data=successdata, family=binomial)
+	newdata1 <- with(successdata, data.frame(saleintended2=1))
 	newdata2 <- predict(mylogit, newdata=newdata1, type="response", se.fit=TRUE)
 	efit = newdata2$fit
 	lfit = efit - (newdata2$se.fit * 1.96)
