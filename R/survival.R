f4ea51f8c639c3ed966af9e77aa25abe4b5762e8
David Schnell-Davis <dschnelldavis@gmail.com>
2014-09-11 13:14:55 -0700

    update

--- R/survival.R
+++ R/survival.R
@@ -1,30 +1,16 @@
-#' survival
+#' Survival
 #' 
-#' Test survival function
+#' Bankruptcy survival function
 #' 
 #' @export
-#' @param saleintended2. Optional.
-survival <- function(saleintended2var = NaN) {
-
-	# The variables names after "~" come from the calculator input.
-	mylogit <- glm(emerge ~ saleintended2 + ebitbeforedummy + equitybefore +  manufacturing, data=successdata, family=binomial)
-	#	summary(mylogit)
-
-	# The values after "=" come from the calculator input.
-	newdata1 <- with(successdata, data.frame(saleintended2=0, equitybefore=0, ebitbeforedummy=1, manufacturing=1))
+#' @param input. Required.
+survival <- function(input) {
+	inputdata <- as.data.frame(input)
+	mylogit <- glm(emerge ~ inputdata, data=successdata, family=binomial)
+	newdata1 <- with(successdata, inputdata)
 	newdata2 <- predict(mylogit, newdata=newdata1, type="response", se.fit=TRUE)
-
-	# This is the predicted probability of Success
 	efit = newdata2$fit
-
-	# These are the 95% lower and upper confidence intervals of efit
 	lfit = efit - (newdata2$se.fit * 1.96)
 	hfit = efit + (newdata2$se.fit * 1.96)
-
-	# These are the values that should be returned to the user.
-	#	efit # This is the predicted value
-	#	lfit # This is the lower bound
-	#	hfit # This is the upper bound
-
 	list(efit=efit, hfit=hfit, lfit=lfit)
 }
