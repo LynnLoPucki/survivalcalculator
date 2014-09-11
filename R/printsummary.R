a00117b0b21ca85effe8be368ed9974830472a57
David Schnell-Davis <dschnelldavis@gmail.com>
2014-09-10 17:29:35 -0700

    Change printsummary back

--- R/printsummary.R
+++ R/printsummary.R
@@ -6,8 +6,8 @@
 #' @param mydata some object or dataset
 printsummary <- function(mydata){
   #override default 
-  #options(max.print=99999999);
-  #options(width=120);
+  options(max.print=99999999);
+  options(width=120);
   
   #print
   print(summary(mydata))
