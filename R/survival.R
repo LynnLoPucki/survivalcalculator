1fd62c28cf5ea8743fc46551a549d1ae220c95bf
David Schnell-Davis <dschnelldavis@gmail.com>
2014-09-11 20:51:47 -0700

    Updated function

--- R/survival.R
+++ R/survival.R
@@ -4,10 +4,11 @@
 #' 
 #' @export
 #' @param input. Required.
-survival <- function(input) {
-	inputdata <- as.data.frame(input)
-	mylogit <- glm(emerge ~ names(inputdata), data=successdata, family=binomial)
-	newdata1 <- with(successdata, inputdata)
+survival <- function(formula, values) {
+	useformula <- as.formula(formula)
+	usevalues <- as.data.frame(values)
+	mylogit <- glm(useformula, data=successdata, family=binomial)
+	newdata1 <- with(successdata, usevalues)
 	newdata2 <- predict(mylogit, newdata=newdata1, type="response", se.fit=TRUE)
 	efit = newdata2$fit
 	lfit = efit - (newdata2$se.fit * 1.96)
