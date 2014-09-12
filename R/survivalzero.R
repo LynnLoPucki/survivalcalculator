f3eee28addef0630b8981c09554b72ef6d12a5a5
David Schnell-Davis <dschnelldavis@gmail.com>
2014-09-11 20:53:26 -0700

    update

--- R/survivalzero.R
+++ R/survivalzero.R
@@ -1,4 +1,4 @@
-survival <- function() {
+survivalzero <- function() {
 	mylogit <- glm(emerge ~ saleintended2 + ebitbeforedummy + equitybefore +  manufacturing, data=successdata, family=binomial)
 	newdata1 <- with(successdata, data.frame(saleintended2=0, equitybefore=0, ebitbeforedummy=1, manufacturing=1))
 	newdata2 <- predict(mylogit, newdata=newdata1, type="response", se.fit=TRUE)
