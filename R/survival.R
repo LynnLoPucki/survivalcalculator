67a6af5cd8542a335782a82094f04f3b588e69b8
David Schnell-Davis <dschnelldavis@gmail.com>
2014-09-11 20:55:11 -0700

    update

--- R/survival.R
+++ R/survival.R
@@ -4,9 +4,9 @@
 #' 
 #' @export
 #' @param input. Required.
-survival <- function(formula, values) {
-	useformula <- as.formula(formula)
-	usevalues <- as.data.frame(values)
+survival <- function(inputformula, inputvalues) {
+	useformula <- as.formula(inputformula)
+	usevalues <- as.data.frame(inputvalues)
 	mylogit <- glm(useformula, data=successdata, family=binomial)
 	newdata1 <- with(successdata, usevalues)
 	newdata2 <- predict(mylogit, newdata=newdata1, type="response", se.fit=TRUE)
